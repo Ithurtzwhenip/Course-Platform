3a15039e038ea62bfa874316388d18faca9e1026
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-03-01 22:49:36 +0530

    baseline course design

---
+++ src/courses/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+# Create your models here.
+class AccessRequirement(models.TextChoices):
+    PUBLISHED = "any", "Anyone"
+    EMAIL_REQUIRED = "email_required", "Email Required"
+
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "pub", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
+    access = models.CharField(max_length=10,
+                              choices=AccessRequirement.choices,
+                              default=AccessRequirement.EMAIL_REQUIRED
+
+                              )
+
+    status = models.CharField(max_length=10,
+                              choices=PublishStatus.choices,
+                              default=PublishStatus.DRAFT
+
+                              )
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
