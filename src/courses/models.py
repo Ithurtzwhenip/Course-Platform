a73a2936093592f87fe26dc7042f8a04edb860e0
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2025-03-02 21:49:30 +0530

    new models.py

--- src/courses/models.py
+++ src/courses/models.py
@@ -3,6 +3,8 @@ import helpers
 from cloudinary.models import CloudinaryField
 
 helpers.cloudinary_init()
+
+
 # Create your models here.
 class AccessRequirement(models.TextChoices):
     PUBLISHED = "any", "Anyone"
@@ -39,3 +41,32 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+
+    @property
+    def image_admin_url(self):
+        if not self.image:
+            return None
+        image_options = {
+            "width": 200,
+        }
+        url = self.image.build_url(**image_options)
+        return url
+
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return None
+        image_options = {
+            "width": width,
+        }
+        if as_html:
+            # CloudinaryImage(str(self.image)).image(**image_options)
+            return self.image.build_url(**image_options)
+        # CloudinaryImagestr(str(self.image)).build_url(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+
+class Lesson(models.Model):
+    my_related_obj = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
