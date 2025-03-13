0531ea892bd0aaa3fc8da3abaef41f7eceaf423f
ithurtzwhenip <sinhasahil423@gmail.com>
2025-03-13 20:57:25 +0530

    url view routing

--- src/courseplatform/urls.py
+++ src/courseplatform/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for courseplatform project.
+URL configuration for cfehome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
 from django.conf.urls.static import static
 
+from courses import views as courses_views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("courses/<int:course_id>/lessons/<int:lesson_id>/", courses_views.lesson_detail_view),
+    path("courses/<int:course_id>/", courses_views.course_detail_view),
+    path("courses/", courses_views.course_list_view),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,27 +1,31 @@
-from urllib import request
-from urllib.error import HTTPError
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
+
 from . import services
 
+def course_list_view(request):
+    queryset = services.get_publish_courses()
+    print(queryset)
+    return JsonResponse({"data": [x.id for x in queryset]})
+    return render(request, "courses/list.html", {})
 
-# Create your views here.
 
-def course_list():
-    queryset = services.get_publish_sources()
-    return render(request, 'courses/list.html', {})
-
-
-def course_detail(request, course_id=None, *args, **kwargs):
+def course_detail_view(request, course_id=None, *args, **kwarg):
     course_obj = services.get_course_detail(course_id=course_id)
     if course_obj is None:
         raise Http404
-    return render(request, 'courses/detail.html', {})
+    lessons_queryset = course_obj.lesson_set.all()
+    return JsonResponse({"data": course_obj.id, 'lesson_ids': [x.id for x in lessons_queryset] })
+    return render(request, "courses/detail.html", {})
 
 
-def lesson_detail(request, course_id=None, lesson_id=None, *args, **kwargs):
+def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs):
+    print(course_id, lesson_id)
     lesson_obj = services.get_lesson_detail(
-        course_id=course_id, lesson_id=lesson_id)
+        course_id=course_id,
+        lesson_id=lesson_id
+    )
     if lesson_obj is None:
         raise Http404
-    return render(request, 'courses/lesson.html', {})
+    return JsonResponse({"data": lesson_obj.id })
+    return render(request, "courses/lesson.html", {})
